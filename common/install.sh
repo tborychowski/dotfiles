de5862abc5e86d605aed924004031d57ebb64155
Tomasz Borychowski <tborychowski@gmail.com>
2014-04-22 17:42:09 +0100

    tweaks

--- common/install.sh
+++ common/install.sh
@@ -8,10 +8,10 @@ fi
 git clone git://github.com/robbyrussell/oh-my-zsh.git $HOME/bin/zsh/oh-my-zsh
 
 # Install bash, zsh & rc files
-ln -sf $HOME/bin/common/nanorc.sh $HOME/.nanorc
+ln -sf $HOME/bin/common/nano.nanorc $HOME/.nanorc
 echo -e "[OK] .nanorc"
 
-ln -sf $HOME/bin/common/vimrc.sh $HOME/.vimrc
+ln -sf $HOME/bin/common/vim.vimrc $HOME/.vimrc
 echo -e "[OK] .vimrc"
 
 ln -sf $HOME/bin/bash/bashrc.sh $HOME/.bashrc
@@ -19,9 +19,6 @@ echo -e "[OK] .bashrc"
 
 ln -sf $HOME/bin/zsh/zshrc.sh $HOME/.zshrc
 echo -e "[OK] .zshrc"
-
-ln -sf $HOME/bin/zsh/theme.sh $HOME/bin/zsh/oh-my-zsh/themes/tom.zsh-theme
-echo -e "[OK] tom.zsh-theme"
 
 $HOME/bin/common/install-nano-syntax.sh
 
