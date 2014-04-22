0b70fc5bf0d10f13af52f64370199f62c3ccdcc7
Tomasz Borychowski <tborychowski@gmail.com>
2014-04-22 10:04:05 +0100

    nanos

--- common/install.sh
+++ common/install.sh
@@ -26,8 +26,7 @@ echo -e "\e[32m[OK]\e[39m tom.zsh-theme"
 if [ ! -d /usr/share/nano ]; then
     sudo mkdir /usr/share/nano
 fi
-sudo cp -f $HOME/bin/common/nano-syntax/*.nanorc /usr/share/nano
-# ls /usr/share/nano/*.nanorc | xargs -I {} echo 'include "{}"' >> $HOME/bin/common/nanorc.sh
+# ls $HOME/bin/common/nano-syntax/*.nanorc | xargs -I {} echo 'include "{}"' >> $HOME/bin/common/nanorc.sh
 echo -e "\e[32m[OK]\e[39m nano syntax highlighting"
 
 echo "Don't forget to install fonts: https://github.com/Lokaltog/powerline-fonts"
