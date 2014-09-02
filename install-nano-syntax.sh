291c3022e785d1e7981b8c1b8d0a2b2f5de324eb
Tom <tborychowski@gmail.com>
2014-09-02 18:55:31 +0100

    refactoring

--- install-nano-syntax.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-sed '/^include/d' $HOME/bin/common/nanorc.sh > $HOME/bin/common/nanorc.tmp
-cd $HOME/bin/common/nano-syntax
-ls *.nanorc | xargs -I {} echo 'include "~/bin/common/nano-syntax/{}"' >> $HOME/bin/common/nanorc.tmp
-mv $HOME/bin/common/nanorc.tmp $HOME/bin/common/nanorc.sh
-echo "[OK] nano syntax highlighting"
--- install.sh
+++ install.sh
@@ -1,14 +1,22 @@
 #!/bin/bash
 
 # Install bash, zsh & rc files
-ln -sf $HOME/bin/common/nanorc.sh $HOME/.nanorc
-echo -e "[OK] .nanorc"
-
-ln -sf $HOME/bin/common/vimrc.sh $HOME/.vimrc
-echo -e "[OK] .vimrc"
-
-ln -sf $HOME/bin/bash/bashrc.sh $HOME/.bashrc
+ln -sf $HOME/bin/dotfiles/bashrc.sh $HOME/.bashrc
 echo -e "[OK] .bashrc"
 
-$HOME/bin/common/install-nano-syntax.sh
+ln -sf $HOME/bin/dotfiles/nanorc.sh $HOME/.nanorc
+echo -e "[OK] .nanorc"
+
+ln -sf $HOME/bin/dotfiles/vimrc.sh $HOME/.vimrc
+echo -e "[OK] .vimrc"
+
+
+
+
+# install NANO syntax highlighting
+sed '/^include/d' $HOME/bin/dotfiles/nanorc.sh > $HOME/bin/dotfiles/nanorc.tmp
+cd $HOME/bin/dotfiles/nano-syntax
+ls *.nanorc | xargs -I {} echo 'include "~/bin/dotfiles/nano-syntax/{}"' >> $HOME/bin/dotfiles/nanorc.tmp
+mv $HOME/bin/dotfiles/nanorc.tmp $HOME/bin/dotfiles/nanorc.sh
+echo "[OK] nano syntax highlighting"
 
